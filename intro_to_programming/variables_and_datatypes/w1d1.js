5c20ed94e3b96e86f9b0acf50f4a1c992c7b00cf
Narciso Lobo <narcisolobo@gmail.com>
2021-07-07 16:13:53 -0500

    initial commit

---
+++ intro_to_programming/variables_and_datatypes/w1d1.js
@@ -0,0 +1,46 @@
+// Rules when naming a variable
+// No spaces
+// Cannot have a hyphen
+// Cannot start with a number
+
+var faveColor = 'Orange'
+console.log("Before reassignment " + faveColor)
+faveColor = "Blue"
+console.log("After reassignment " + faveColor)
+var yourFaveColor = "Yellow"
+
+// Concatenation
+console.log("Concatenated " + faveColor + " " + yourFaveColor)
+
+// Camel Case - JavaScript naming convention for variables and functions where the first letter is lowercase and each successive word thereafter starts with an uppercase letter.
+
+// Primitive Datatypes - One thing.
+
+// Strings
+// Strings of characters
+
+// Numbers
+// In JS, decimals and integers are both "Numbers"
+
+var pi = 3.14
+var anotherNumber = 10
+var stringNumber = "10"
+var result = pi + anotherNumber
+console.log(result)
+// Arithmetic operators
+// +, -, *, /
+
+// Booleans
+// true, or false
+
+var isSunny = true
+isSunny = false
+console.log(isSunny)
+
+// null
+var choice = null
+
+// undefined
+var toBeDetermined
+
+console.log(anotherNumber + yourFaveColor)
